29a905c69533d9f6d04c8dba980d6f0ea8d23dba
Moktadir Rahman <autocircled@gmail.com>
2020-07-13 23:47:01 +0600

    Theme option added and header part working with theme option

--- header.php
+++ header.php
@@ -28,20 +28,25 @@
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
 			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
+                        $get_option = get_option('autocircle_theme_settings');
+			if( $get_option['show_logo'] == 'yes' ) the_custom_logo();
+                        if( $get_option['title'] == 'yes' ) :
+                            if ( is_front_page() && is_home() ) :
+                                    ?>
+                                    <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+                                    <?php
+                            else :
+                                    ?>
+                                    <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+                                    <?php
+                            endif;
 			endif;
-			$autocircle_description = get_bloginfo( 'description', 'display' );
-			if ( $autocircle_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $autocircle_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+                        if( $get_option['description'] == 'yes' ) :
+                            $autocircle_description = get_bloginfo( 'description', 'display' );
+                            if ( $autocircle_description || is_customize_preview() ) :
+                                    ?>
+                                    <p class="site-description"><?php echo $autocircle_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+                            <?php endif; ?>
 			<?php endif; ?>
 		</div><!-- .site-branding -->
 
--- inc/theme-options.php
+++ inc/theme-options.php
@@ -10,6 +10,17 @@ if( !function_exists( 'create_theme_settings_menu' ) ){
 
 if( !function_exists( 'autocircle_option_page' ) ){
     function autocircle_option_page(){
+        
+        $values = filter_input_array(INPUT_POST);
+        $config = $values['ac_theme_option'];
+        if( isset($values['submit']) ){
+            
+            update_option( 'autocircle_theme_settings', $config );
+            
+        }
+        $get_option = get_option('autocircle_theme_settings');
+
+        
         ?>
 <div class="wrap">
     <div class="option-holder">
@@ -28,16 +39,43 @@ if( !function_exists( 'autocircle_option_page' ) ){
                 </div>
             </div>
         </div>
-        <h1>Custom Theme Options Page</h1>
-        <form method="post" action="options.php">
-        <?php
-        // display settings field on theme-option page
-        settings_fields("theme-options-grp");
-
-        // display all sections for theme-options page
-        do_settings_sections("autocircle-options");
-        submit_button();
-        ?>
+        <form method="post" action="">
+            <table>
+                <tr>
+                    <th>Logo</th>
+                    <td>
+                        <input type="radio" id="show_logo_no" name="ac_theme_option[show_logo]" value="no" <?php echo isset($get_option['show_logo']) && $get_option['show_logo'] == 'no' ? 'checked' : ''; ?>><label for="show_logo_no">No</label>
+                        <input type="radio" id="show_logo_yes" name="ac_theme_option[show_logo]" value="yes" <?php echo isset($get_option['show_logo']) && $get_option['show_logo'] == 'yes' ? 'checked' : ''; ?>><label for="show_logo_yes">Yes</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Title</th>
+                    <td>
+                        <input type="radio" id="title_no" name="ac_theme_option[title]" value="no" <?php echo isset($get_option['title']) && $get_option['title'] == 'no' ? 'checked' : ''; ?>><label for="title_no">No</label>
+                        <input type="radio" id="title_yes" name="ac_theme_option[title]" value="yes" <?php echo isset($get_option['title']) && $get_option['title'] == 'yes' ? 'checked' : ''; ?>><label for="title_yes">Yes</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Description</th>
+                    <td>
+                        <input type="radio" id="description_no" name="ac_theme_option[description]" value="no" <?php echo isset($get_option['description']) && $get_option['description'] == 'no' ? 'checked' : ''; ?>><label for="description_no">No</label>
+                        <input type="radio" id="description_yes" name="ac_theme_option[description]" value="yes" <?php echo isset($get_option['description']) && $get_option['description'] == 'yes' ? 'checked' : ''; ?>><label for="description_yes">Yes</label>
+                    </td>
+                </tr>
+                <tr>
+                    <th>Background</th>
+                    <td>
+                        <input type="radio" id="background_no" name="ac_theme_option[background]" value="no" <?php echo isset($get_option['background']) && $get_option['background'] == 'no' ? 'checked' : ''; ?>><label for="background_no">No</label>
+                        <input type="radio" id="background_yes" name="ac_theme_option[background]" value="yes" <?php echo isset($get_option['background']) && $get_option['background'] == 'yes' ? 'checked' : ''; ?>><label for="background_yes">Yes</label>
+                    </td>
+                </tr>
+                
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" name="submit" class="button primary" value="Save Settings" />
+                    </td>
+                </tr>
+            </table>
         </form>
     </div>
 </div>
